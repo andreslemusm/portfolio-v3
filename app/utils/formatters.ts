3da2201451ceb66fa9cb3d931917654bdd697678
Andres Lemus <aclmadrid04@gmail.com>
2023-11-09 22:25:42 -0500

    refactor: remove unneccesary code

--- app/utils/formatters.ts
+++ app/utils/formatters.ts
@@ -1,7 +1,4 @@
-const classNames = (...classes: Array<string | boolean>): string =>
-  classes.filter(Boolean).join(" ");
-
 const clamp = (number: number, a: number, b: number): number =>
   Math.min(Math.max(number, Math.min(a, b)), Math.max(a, b));
 
-export { classNames, clamp };
+export { clamp };
