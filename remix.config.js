3a66dd237a6edd0eaed681d044e2a3064f0df1b1
Andres Lemus <aclmadrid04@gmail.com>
2024-01-16 19:31:16 -0500

    feat: update deps

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  future: {
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true,
+  },
 };
