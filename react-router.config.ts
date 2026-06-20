f46091d1f2f13b10aa73b1e05de9168244f0f5d7
Andres Lemus <aclmadrid04@gmail.com>
2026-06-20 09:29:22 -0500

    feat: enable rr future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,15 @@ import type { Config } from "@react-router/dev/config"
 import { vercelPreset } from "@vercel/react-router/vite"
 
 export default {
-  future: { unstable_optimizeDeps: true },
+  future: {
+    unstable_optimizeDeps: true,
+    unstable_previewServerPrerendering: true,
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests: true,
+    v8_trailingSlashAwareDataRequests: true,
+  },
   ssr: true,
   presets: [vercelPreset()],
 } satisfies Config
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: { tsconfigPaths: true },
+  plugins: [tailwindcss(), reactRouter()],
 })
